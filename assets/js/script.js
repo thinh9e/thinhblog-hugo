962fb29313f82613e39244a34ff151df688f9049
Thinh Nguyen Phuoc <npthinh1996@gmail.com>
2020-11-29 22:45:21 +0700

    custom button navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,12 @@ $(document).ready(function () {
     $("#back-top").click(function () {
         $(window).scrollTop(0);
     })
+
+    $(".navbar-toggler").click(function () {
+        if ($(this).attr("aria-expanded") == "true") {
+            $(this).children("i").removeClass("fa-times").addClass("fa-bars");
+        } else {
+            $(this).children("i").removeClass("fa-bars").addClass("fa-times");
+        }
+    })
 })
